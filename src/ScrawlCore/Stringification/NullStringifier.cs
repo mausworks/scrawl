5a269d9e23858249ecb9a58b465e0883faa8e31e
mausworks <me@fixz.se>
2016-10-08 19:12:35 +0200

    NoopStringifier is now NullStringifier.

---
+++ src/ScrawlCore/Stringification/NullStringifier.cs
@@ -0,0 +1,17 @@
+﻿namespace ScrawlCore.Stringification
+{
+    /// <summary>
+    /// Stringifies objects by returning a null reference. 
+    /// This stringifier can stringify any type and is considered no-op.
+    /// </summary>
+    public sealed class NullStringifier : AnyTypeStringifier
+    {
+        /// <summary>
+        /// Returns a null reference.
+        /// </summary>
+        /// <param name="value">[ignored]</param>
+        /// <returns>a null reference./returns>
+        public override string Stringify(object value)
+            => null;
+    }
+}
